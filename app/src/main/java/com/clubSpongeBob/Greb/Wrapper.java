a568a7be54ca9a73a18df836424eadce230a359c
QuackQuack <63900029+nwjun@users.noreply.github.com>
2022-06-04 11:55:52 +0800

    Minor changes

--- app/src/main/java/com/clubSpongeBob/Greb/Wrapper.java
+++ app/src/main/java/com/clubSpongeBob/Greb/Wrapper.java
@@ -1,5 +1,6 @@
 package com.clubSpongeBob.Greb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -7,18 +8,26 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Wrapper extends AppCompatActivity{
+    final String TAG = "Wrapper";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent;
-        if(FirebaseUtils.isLogin(this)){
+        setContentView(R.layout.activity_wrapper);
+
+        final Intent intent;
+        if(FirebaseUtils.isLogin(getApplicationContext())){
             intent = new Intent(getApplication(), AdminLanding.class);
-        }else{
+            Log.i(TAG, "Authenticated");
+        } else{
             intent = new Intent(getApplication(), AuthLanding.class);
+            Log.i(TAG, "Not Authenticated");
         }
         startActivity(intent);
     }
