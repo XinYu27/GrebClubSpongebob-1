48d271d83a62b9bfb123681c9cfc81ae65a8c1f6
tiansien <tiansien02@gmail.com>
2022-06-11 05:16:26 +0800

    all ui done

---
+++ app/src/main/java/com/clubSpongeBob/Greb/driver_status.java
@@ -0,0 +1,13 @@
+package com.clubSpongeBob.Greb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class driver_status extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_status);
+    }
+}
