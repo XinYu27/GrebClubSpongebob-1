a7b7e9508e61e16a8e62d4cfc04d6041e8d8902e
tiansien <tiansien02@gmail.com>
2022-06-13 15:37:44 +0800

    add driver
    ui update and the add driver functionable

--- app/src/main/java/com/clubSpongeBob/Greb/AddDriver.java
+++ app/src/main/java/com/clubSpongeBob/Greb/AddDriver.java
@@ -1,12 +1,17 @@
 package com.clubSpongeBob.Greb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -17,8 +22,7 @@ public class AddDriver extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
-
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_driver);
 
@@ -26,6 +30,7 @@ public class AddDriver extends AppCompatActivity {
         driver_Name = (EditText) findViewById(R.id.driver_Name);
         car_Modal = findViewById(R.id.car_Model);
         car_Capacity = findViewById(R.id.car_Capacity);
+        car_Plate = findViewById(R.id.car_Plate);
         driver_Location = findViewById(R.id.driver_Location);
         car_Colour = findViewById(R.id.car_Colour);
         databaseadddrivers = FirebaseDatabase.getInstance().getReference();
@@ -40,6 +45,27 @@ public class AddDriver extends AppCompatActivity {
     }
 
     private void Adddriver() {
-        String drivername = driver_name
+        String drivername = driver_Name.getText().toString();
+        String carmodal = car_Modal.getText().toString();
+        String carcapacity = car_Capacity.getText().toString();
+        String carplate = car_Plate.getText().toString();
+        String driverlocation = driver_Location.getText().toString();
+        String carcolour = car_Colour.getText().toString();
+        String id = databaseadddrivers.push().getKey();
+
+        int carr_capacity = Integer.parseInt(carcapacity);
+
+        Driver driver = new Driver(drivername,carmodal,carplate,carcolour,carr_capacity,driverlocation);
+        databaseadddrivers.child("drivers").child(id).setValue(driver)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(AddDriver.this, "Driver have been inserted", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(AddDriver.this,AddDriver.class);
+                            startActivity(intent);
+                        }
+                    }
+                });
     }
 }
