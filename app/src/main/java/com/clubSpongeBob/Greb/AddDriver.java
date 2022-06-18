79dacc83edcfcba966884de0b0bcf146fe8f1741
QuackQuack <63900029+nwjun@users.noreply.github.com>
2022-06-18 19:24:25 +0800

    Revert "recycle driver view"
    This reverts commit a00a37a94bfdaf11c636726fe6fb7b2c80730d60.

--- app/src/main/java/com/clubSpongeBob/Greb/AddDriver.java
+++ app/src/main/java/com/clubSpongeBob/Greb/AddDriver.java
@@ -22,7 +22,7 @@ public class AddDriver extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_driver);
 
