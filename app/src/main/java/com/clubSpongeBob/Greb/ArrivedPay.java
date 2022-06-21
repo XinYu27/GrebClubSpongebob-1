6e33bf4ecdc45e48c9043187695aa862d871bd22
tiansien <tiansien02@gmail.com>
2022-06-22 02:11:25 +0800

    update ui and the arrived page

--- app/src/main/java/com/clubSpongeBob/Greb/ArrivedPay.java
+++ app/src/main/java/com/clubSpongeBob/Greb/ArrivedPay.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,17 +17,22 @@ public class ArrivedPay extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arrived_pay);
 
-        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setDisplayShowCustomEnabled(true);
-        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+        try{
+            this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+            getSupportActionBar().setDisplayShowCustomEnabled(true);
+            getSupportActionBar().setCustomView(R.layout.custom_action_bar);
 
-        View view = getSupportActionBar().getCustomView();
-        ImageView imageView = view.findViewById(R.id.backNavigation);
+            View view = getSupportActionBar().getCustomView();
+            ImageView imageView = view.findViewById(R.id.backNavigation);
 
-        imageView.setVisibility(View.GONE);
+            imageView.setVisibility(View.GONE);
 
-        TextView name = view.findViewById(R.id.name);
-        name.setText("Arrived and pay");
+            TextView name = view.findViewById(R.id.name);
+            name.setText("Arrived and pay");
+        } catch (Exception e){
+            Log.e("ArrivedPay", e.getMessage());
+        }
+
         TextView amountView = this.findViewById(R.id.amountTextView);
         amountView.setText(String.format("RM%.2f", CommonUtils.getSelectedDriver().getTotalDistance()/1000 * 0.5 ));
 
